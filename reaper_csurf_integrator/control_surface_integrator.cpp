88e7b1c2c1eb730e1d6002eb8222d45ccf2bb8d6
smarty56 <144461347+smarty56@users.noreply.github.com>
2025-07-15 07:08:15 +0100

    Added class V1MVUMeter_Midi_FeedbackProcessor

--- reaper_csurf_integrator/control_surface_integrator.cpp
+++ reaper_csurf_integrator/control_surface_integrator.cpp
@@ -620,6 +620,12 @@ void Midi_ControlSurface::ProcessMidiWidget(int &lineNumber, ifstream &surfaceTe
         {
             widget->GetFeedbackProcessors().push_back(make_unique<QConProXMasterVUMeter_Midi_FeedbackProcessor>(csi_, this, widget, atoi(tokenLines[i][1].c_str())));
         }
+        else if ((widgetType == "FB_V1MVUMeter" || widgetType == "FB_V1MXVUMeter") && size == 2) //Kev Smart
+        {
+            int displayType = widgetType == "FB_V1MVUMeter" ? 0x14 : 0x15;
+            widget->GetFeedbackProcessors().push_back(make_unique<V1MVUMeter_Midi_FeedbackProcessor>(csi_, this, widget, displayType, atoi(tokenLines[i][1].c_str())));
+            SetHasMCUMeters(displayType);
+        }
         else if ((widgetType == "FB_MCUVUMeter" || widgetType == "FB_MCUXTVUMeter") && size == 2)
         {
             int displayType = widgetType == "FB_MCUVUMeter" ? 0x14 : 0x15;
